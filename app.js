8f68c4072071bd8a01f5f1b48217815aca308854
dev_clemeno <dev.francesco.clemeno@outlook.it>
2024-04-28 17:11:51 +0200

    another start commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.type('text').send("The Alessandro Zurzolo Server is running..."));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
